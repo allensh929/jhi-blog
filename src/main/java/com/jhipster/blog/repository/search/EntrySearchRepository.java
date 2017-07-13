17f0f0028dd878c5c6e5dbccbf759cba1de53df0
Chen Li <chen.li@blackboard.com>
2017-07-13 13:34:58 +0800

    Generated with JHipster 4.5.6

---
+++ src/main/java/com/jhipster/blog/repository/search/EntrySearchRepository.java
@@ -0,0 +1,10 @@
+package com.jhipster.blog.repository.search;
+
+import com.jhipster.blog.domain.Entry;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the Entry entity.
+ */
+public interface EntrySearchRepository extends ElasticsearchRepository<Entry, Long> {
+}
